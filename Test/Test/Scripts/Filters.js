e113a53bcbb0c1711cfb0126562681c5ff5c3261
gaurav-lad <gaurav.lad02@gmail.com>
2018-01-27 16:29:53 +0530

    First Commit

---
+++ Test/Test/Scripts/Filters.js
@@ -0,0 +1,14 @@
+/// <references path="Scripts.js"/>
+
+myApp.filter("gender", function (){
+    return function (gender) {
+        switch (gender) {
+            case 1:
+            return "male";
+            case 2:
+            return "female";
+            case 3:
+            return "not disclosed";
+        }
+    }
+})
